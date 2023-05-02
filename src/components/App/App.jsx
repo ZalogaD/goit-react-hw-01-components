24e39ccd48dfb875b967552a52c499d454ca9596
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-05-02 17:30:06 +0300

    hw

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import { AppContainer } from './App.styled';
+import { Profile } from 'components/Profile/Profile';
+import user from 'data/user';
+import { Statistics } from 'components/Statistics/Statistics';
+import data from 'data/data';
+import { FriendsList } from 'components/FriendList/FriendList';
+import friends from 'data/friends';
+import { TransactionHistory } from 'components/TransitionHistory/TransitionHistory';
+import transactions from 'data/transactions';
+export const App = () => {
+  return (
+    <AppContainer>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </AppContainer>
+  );
+};
